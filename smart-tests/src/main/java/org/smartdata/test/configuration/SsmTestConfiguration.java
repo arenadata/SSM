739881bae79915007ba12e2e1bdd1bb995a98100
Viacheslav Vasiliev <v.vasiliev@arenadata.io>
2025-06-06 19:18:57 +0300

    ADH-5881: Use Generic container to start test env

--- smart-tests/src/main/java/org/smartdata/test/configuration/SsmTestConfiguration.java
+++ smart-tests/src/main/java/org/smartdata/test/configuration/SsmTestConfiguration.java
@@ -19,6 +19,7 @@ package org.smartdata.test.configuration;
 
 import io.arenadata.test.configuration.CommonTestConfiguration;
 import io.arenadata.test.model.Component;
+import io.arenadata.test.service.ContainerManager;
 import io.arenadata.test.service.HostService;
 import io.arenadata.test.service.SshCommandExecutor;
 import io.arenadata.test.service.impl.DockerComposeService;
@@ -63,8 +64,8 @@ public class SsmTestConfiguration {
     @Bean("hostService")
     @ConfigurationProperties(prefix = "docker-compose-service")
     @ConditionalOnProperty(name = "env-type", havingValue = "docker")
-    public HostService dockerHostService(List<Component> ssmComponents) {
-        return new DockerComposeService(ssmComponents);
+    public HostService dockerHostService(ContainerManager containerManager) {
+        return new DockerComposeService(containerManager);
     }
 
     @Bean("hostService")
