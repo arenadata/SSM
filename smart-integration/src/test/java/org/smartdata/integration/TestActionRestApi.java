a3d482a2621e287e4c42f668a0e339af43ce00e7
Oleg Popko <aleh.papko@gmail.com>
2025-07-27 22:40:51 +0300

    flaky integration test fix + refactor timeouts

--- smart-integration/src/test/java/org/smartdata/integration/TestActionRestApi.java
+++ smart-integration/src/test/java/org/smartdata/integration/TestActionRestApi.java
@@ -49,6 +49,8 @@ public class TestActionRestApi extends IntegrationTestBase {
   private RulesApiWrapper rulesApi;
 
   private static final String ACTION_TEXT = "sleep -ms 10";
+  private static final Duration INTERVAL = Duration.ofMillis(100);
+  private static final Duration TIMEOUT = Duration.ofSeconds(5);
 
   @Before
   public void createApi() {
@@ -67,7 +69,7 @@ public class TestActionRestApi extends IntegrationTestBase {
     assertEquals(ActionSourceDto.USER, actionInfo.getSource());
 
     ActionDto actionDto = apiClient.waitTillActionFinished(actionInfo.getId(),
-        Duration.ofMillis(100L), Duration.ofMillis(1000L));
+        INTERVAL, TIMEOUT);
 
     assertEquals(actionInfo.getId(), actionDto.getId());
     assertEquals(actionText, actionDto.getTextRepresentation());
@@ -89,7 +91,7 @@ public class TestActionRestApi extends IntegrationTestBase {
 
     // we have to wait a bit, because of async cmdlets transfer
     // from in-memory cache to metastore
-    apiClient.waitActionsTotalSize(1, Duration.ofMillis(100), Duration.ofSeconds(1));
+    apiClient.waitActionsTotalSize(1, INTERVAL, TIMEOUT);
 
     actions = apiClient.getActions();
     assertEquals(1L, actions.getItems().size());
@@ -110,7 +112,7 @@ public class TestActionRestApi extends IntegrationTestBase {
     apiClient.submitAction(ACTION_TEXT);
     ActionInfoDto action = apiClient.submitAction(ACTION_TEXT);
 
-    apiClient.waitActionsTotalSize(2, Duration.ofMillis(100), Duration.ofSeconds(1));
+    apiClient.waitActionsTotalSize(2, INTERVAL, TIMEOUT);
 
     ActionsDto actions = apiClient.rawClient()
         .getActions()
@@ -131,7 +133,7 @@ public class TestActionRestApi extends IntegrationTestBase {
     ActionInfoDto firstAction = apiClient.submitAction(ACTION_TEXT);
     ActionInfoDto secondAction = apiClient.submitAction(ACTION_TEXT);
 
-    apiClient.waitActionsTotalSize(2, Duration.ofMillis(100), Duration.ofSeconds(1));
+    apiClient.waitActionsTotalSize(2, INTERVAL, TIMEOUT);
 
     // ASC
     ActionsDto actions = apiClient.rawClient()
@@ -167,7 +169,7 @@ public class TestActionRestApi extends IntegrationTestBase {
     ActionInfoDto firstAction = apiClient.submitAction(ACTION_TEXT);
     ActionInfoDto secondAction = apiClient.submitAction(ACTION_TEXT);
 
-    apiClient.waitActionsTotalSize(2, Duration.ofMillis(100), Duration.ofSeconds(1));
+    apiClient.waitActionsTotalSize(2, INTERVAL, TIMEOUT);
 
     // ASC
     ActionsDto actions = apiClient.rawClient()
@@ -201,11 +203,11 @@ public class TestActionRestApi extends IntegrationTestBase {
   @Test
   public void testGetActionsSortByStartTime() {
     ActionInfoDto firstAction = apiClient.submitAction(ACTION_TEXT);
-    apiClient.waitTillActionFinished(firstAction.getId(), Duration.ofMillis(100L), Duration.ofMillis(1000L));
+    apiClient.waitTillActionFinished(firstAction.getId(), INTERVAL, TIMEOUT);
     ActionInfoDto secondAction = apiClient.submitAction(ACTION_TEXT);
-    apiClient.waitTillActionFinished(secondAction.getId(), Duration.ofMillis(100L), Duration.ofMillis(1000L));
+    apiClient.waitTillActionFinished(secondAction.getId(), INTERVAL, TIMEOUT);
 
-    apiClient.waitActionsTotalSize(2, Duration.ofMillis(100), Duration.ofSeconds(1));
+    apiClient.waitActionsTotalSize(2, INTERVAL, TIMEOUT);
 
     // ASC
     ActionsDto actions = apiClient.rawClient()
@@ -240,10 +242,10 @@ public class TestActionRestApi extends IntegrationTestBase {
   @Test
   public void testGetActionsSortByCompletionTime() {
     ActionInfoDto firstAction = apiClient.submitAction(ACTION_TEXT);
-    apiClient.waitTillActionFinished(firstAction.getId(), Duration.ofMillis(100L), Duration.ofMillis(1000L));
+    apiClient.waitTillActionFinished(firstAction.getId(), INTERVAL, TIMEOUT);
     ActionInfoDto secondAction = apiClient.submitAction(ACTION_TEXT);
 
-    apiClient.waitActionsTotalSize(2, Duration.ofMillis(100), Duration.ofSeconds(1));
+    apiClient.waitActionsTotalSize(2, INTERVAL, TIMEOUT);
 
     // ASC
     ActionsDto actions = apiClient.rawClient()
@@ -279,7 +281,7 @@ public class TestActionRestApi extends IntegrationTestBase {
     ActionInfoDto action = apiClient.submitAction(ACTION_TEXT);
     apiClient.submitAction("read -file /tmp/test.txt");
 
-    apiClient.waitActionsTotalSize(2, Duration.ofMillis(100), Duration.ofSeconds(1));
+    apiClient.waitActionsTotalSize(2, INTERVAL, TIMEOUT);
 
     ActionsDto actions = apiClient.rawClient()
         .getActions()
@@ -303,7 +305,7 @@ public class TestActionRestApi extends IntegrationTestBase {
     long end = System.currentTimeMillis();
     apiClient.submitAction(ACTION_TEXT);
 
-    apiClient.waitActionsTotalSize(2, Duration.ofMillis(100), Duration.ofSeconds(1));
+    apiClient.waitActionsTotalSize(2, INTERVAL, TIMEOUT);
 
     ActionsDto actions = apiClient.rawClient()
         .getActions()
@@ -326,11 +328,11 @@ public class TestActionRestApi extends IntegrationTestBase {
   public void testGetActionsFilterByStartTime() {
     long start = System.currentTimeMillis();
     ActionInfoDto action = apiClient.submitAction(ACTION_TEXT);
-    apiClient.waitTillActionFinished(action.getId(), Duration.ofMillis(100L), Duration.ofMillis(1000L));
+    apiClient.waitTillActionFinished(action.getId(), INTERVAL, TIMEOUT);
     long end = System.currentTimeMillis();
     apiClient.submitAction(ACTION_TEXT);
 
-    apiClient.waitActionsTotalSize(2, Duration.ofMillis(100), Duration.ofSeconds(1));
+    apiClient.waitActionsTotalSize(2, INTERVAL, TIMEOUT);
 
     ActionsDto actions = apiClient.rawClient()
         .getActions()
@@ -353,11 +355,11 @@ public class TestActionRestApi extends IntegrationTestBase {
   public void testGetActionsFilterByCompletionTime() {
     long start = System.currentTimeMillis();
     ActionInfoDto action = apiClient.submitAction(ACTION_TEXT);
-    apiClient.waitTillActionFinished(action.getId(), Duration.ofMillis(100L), Duration.ofMillis(1000L));
+    apiClient.waitTillActionFinished(action.getId(), INTERVAL, TIMEOUT);
     long end = System.currentTimeMillis();
     apiClient.submitAction(ACTION_TEXT);
 
-    apiClient.waitActionsTotalSize(2, Duration.ofMillis(100), Duration.ofSeconds(1));
+    apiClient.waitActionsTotalSize(2, INTERVAL, TIMEOUT);
 
     ActionsDto actions = apiClient.rawClient()
         .getActions()
@@ -378,10 +380,10 @@ public class TestActionRestApi extends IntegrationTestBase {
 
   @Test
   public void testGetActionsFilterByStates() {
-    ActionInfoDto action = apiClient.submitAction(ACTION_TEXT);
-    apiClient.submitAction("read -file nonexistent.file");
+    ActionDto action = apiClient.waitTillActionFinished(ACTION_TEXT, INTERVAL, TIMEOUT);
+    apiClient.waitTillActionFinished("read -file nonexistent.file", INTERVAL, TIMEOUT);
 
-    apiClient.waitActionsTotalSize(2, Duration.ofMillis(100), Duration.ofSeconds(5));
+    apiClient.waitActionsTotalSize(2, INTERVAL, TIMEOUT);
 
     ActionsDto actions = apiClient.rawClient()
         .getActions()
@@ -403,10 +405,10 @@ public class TestActionRestApi extends IntegrationTestBase {
     ActionInfoDto action = apiClient.submitAction(ACTION_TEXT);
     rulesApi.waitTillRuleTriggered(
         "file: at now | path matches \"/*\" | sleep -ms 10",
-        Duration.ofMillis(100),
-        Duration.ofSeconds(2));
+        INTERVAL,
+        TIMEOUT);
 
-    apiClient.waitActionsTotalSize(2, Duration.ofMillis(100), Duration.ofSeconds(1));
+    apiClient.waitActionsTotalSize(2, INTERVAL, TIMEOUT);
 
     ActionsDto actions = apiClient.rawClient()
         .getActions()
