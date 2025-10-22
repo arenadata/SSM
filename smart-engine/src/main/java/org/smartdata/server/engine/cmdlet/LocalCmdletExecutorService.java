687e652bcd3aec864a1618efd0c9af315b527178
Tigran Manasyan <t.manasyan@arenadata.io>
2025-10-22 12:55:18 +0400

    [ADH-6995] Fix error in MasterActor (#205)

--- smart-engine/src/main/java/org/smartdata/server/engine/cmdlet/LocalCmdletExecutorService.java
+++ smart-engine/src/main/java/org/smartdata/server/engine/cmdlet/LocalCmdletExecutorService.java
@@ -131,7 +131,7 @@ public class LocalCmdletExecutorService extends CmdletExecutorService implements
   @Override
   public void report(StatusMessage status) {
     LOG.debug("Reporting status message {}", status);
-    cmdletManager.updateStatus(status);
+    cmdletManager.onStatusMessage(status);
   }
 
   private String getActiveServerAddress() {
