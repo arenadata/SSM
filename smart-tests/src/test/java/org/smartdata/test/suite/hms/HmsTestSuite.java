536a1f21e5408b466cd667c6a96a662670fd8a46
Oleg Popko <aleh.papko@gmail.com>
2026-03-04 17:21:15 +0300

    Revert "beforeMethod to afterMethod"
    This reverts commit 91d36e4dc566001c7e4c49a87342ca72316ea903.

--- smart-tests/src/test/java/org/smartdata/test/suite/hms/HmsTestSuite.java
+++ smart-tests/src/test/java/org/smartdata/test/suite/hms/HmsTestSuite.java
@@ -33,6 +33,7 @@ import org.smartdata.test.util.LogsUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import javax.sql.DataSource;
@@ -101,7 +102,7 @@ public class HmsTestSuite extends SsmBaseSuite {
       "org.smartdata.retry.RetryException: try once and fail.";
   private static final Duration AWAITILITY_PULL_INTERVAL = Duration.ofMillis(1000);
 
-  @AfterMethod
+  @BeforeMethod
   public void restoreEnv() throws IOException {
     containerManager.stop(SSM_SERVER);
     configModifierService.restoreOriginalFile(MASTER_CONF_NAME);
